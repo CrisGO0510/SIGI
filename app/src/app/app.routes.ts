09c0a1c46d958c2eb298f99e13df3138b49a7c84
CrisGO <cristhiangiraldo053@gmail.com>
2025-11-22 17:35:21 -0500

    Vista login implementada

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth-guard';
+import { Layout } from './layout/layout';
 
 export const APP_ROUTES: Routes = [
   {
@@ -8,24 +9,10 @@ export const APP_ROUTES: Routes = [
       import('./modules/auth/auth.routes').then((r) => r.AUTH_ROUTES),
   },
   {
-    path: 'dashboard',
-    canActivate: [authGuard],
-    loadChildren: () =>
-      import('./modules/dashboard/dashboard.routes').then(
-        (r) => r.DASHBOARD_ROUTES,
-      ),
-  },
-  {
-    path: 'incapacities',
-    canActivate: [authGuard],
-    loadChildren: () =>
-      import('./modules/incapacities/incapacities.routes').then(
-        (r) => r.INCAPACITIES_ROUTES,
-      ),
-  },
-  {
     path: '',
-    redirectTo: '/auth/login',
-    pathMatch: 'full',
+    component: Layout,
+    children: [
+
+    ],
   },
 ];
--- app/src/app/modules/auth/login/login.ts
+++ app/src/app/modules/auth/login/login.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
   templateUrl: './login.html',
   styleUrls: ['./login.scss'],
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  fb = inject(FormBuilder);
+  router = inject(Router);
+
+  loading = false;
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  submit() {
+    if (this.form.invalid) return;
+
+    this.loading = true;
+
+    // Simulación de login
+    setTimeout(() => {
+      this.loading = false;
+
+      // Guardar token (simulado)
+      localStorage.setItem('token', 'fake-jwt');
+
+      this.router.navigate(['/dashboard']);
+    }, 1200);
+  }
+}
