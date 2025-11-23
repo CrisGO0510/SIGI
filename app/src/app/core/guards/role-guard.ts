eb0f4e48b3aab45463425b819b5fd17b47eef72f
CrisGO <cristhiangiraldo053@gmail.com>
2025-11-23 11:39:19 -0500

    Correción de tipos

--- app/src/app/core/guards/role-guard.ts
+++ app/src/app/core/guards/role-guard.ts
@@ -1,12 +1,17 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { UserService } from '../services/user';
+import { UserRoleEnum } from '../../modules/auth/interfaces/user-role.enum';
 
 export const roleGuard =
-  (allowed: string[]): CanActivateFn =>
+  (allowed: UserRoleEnum[]): CanActivateFn =>
   () => {
     const userService = inject(UserService);
     const role = userService.currentRole();
+
+    if (!role) {
+      return false;
+    }
 
     return allowed.includes(role);
   };
